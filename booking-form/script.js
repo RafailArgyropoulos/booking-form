36a8153518ef177bc35b8969758ef60caf817458
Rafail Argyropoulos <rafailargy@gmail.com>
2025-07-03 11:49:16 +0300

    Add files via upload

---
+++ booking-form/script.js
@@ -0,0 +1,35 @@
+emailjs.init("SP4ADXtsq9ganYnB_");
+
+const form = document.getElementById("booking-form");
+const resultDiv = document.getElementById("result");
+
+const checkinInput = document.getElementById("start-date");
+const checkoutInput = document.getElementById("end-date");
+const nightsInput = document.getElementById("nights");
+
+function calculateNights() {
+  const checkin = new Date(checkinInput.value);
+  const checkout = new Date(checkoutInput.value);
+  const diffTime = checkout - checkin;
+  const nights = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  nightsInput.value = nights > 0 ? nights : 0;
+}
+
+checkinInput.addEventListener("change", calculateNights);
+checkoutInput.addEventListener("change", calculateNights);
+window.addEventListener("load", calculateNights);
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  emailjs.sendForm("service_odbjdsu", "template_4r04dxf", form)
+    .then(() => {
+      resultDiv.textContent = "Message sent successfully!";
+      form.reset();
+      calculateNights();
+    })
+    .catch((error) => {
+      resultDiv.textContent = "Error sending.";
+      console.error(error);
+    });
+});
